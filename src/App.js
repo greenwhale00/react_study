f6d9557112fff64d5aa1e08b66be7481f5792849
greenwhalee00 <goreakay@gmail.com>
2022-10-16 23:40:27 +0900

    221016

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-useState
+
 
 function Header(props) {
   console.log(props);
@@ -38,27 +38,37 @@ function Article(props) {
   </article>
 }
 
-
 function App() {
-  const mode = 'WELCOME';
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
   const topics = [
     { id: "1", title: "html", body: "html is ..." },
     { id: "2", title: "css", body: "css is ..." },
     { id: "3", title: "javascript", body: "js is ..." }
   ]
+  let content = null;
   if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB" />
   } else if (mode === 'READ') {
-    content = <Article title="Read" body="Hello, Read" />
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body} />
   }
   return (
     <div >
       <Header title="WEB" onChangeMode={() => {
-        mode = 'WELCOME'
+        setMode('WELCOME')
       }}
       />
-      <Nav topics={topics} onChangeMode={(id) => {
-        mode = 'READ'
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode('READ');
+        setId(_id);
       }} />
       {content}
 
