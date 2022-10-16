29ceaf4b54e604c16b815c18acd4c10a432d17a4
greenwhalee00 <goreakay@gmail.com>
2022-10-16 19:24:35 +0900

    221016

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+useState
 
 function Header(props) {
   console.log(props);
   return <header>
-    <h1><a href="" onClick={function (event) {
+    <h1><a href="" onClick={(event) => {
       event.preventDefault();
+      props.onChangeMode();
     }}>{props.title}</a></h1>
   </header>
 }
@@ -15,7 +17,12 @@ function Nav(props) {
   const lis = []
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={event => {
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li>)
   }
   return <nav>
     <ol>
@@ -33,19 +40,27 @@ function Article(props) {
 
 
 function App() {
+  const mode = 'WELCOME';
   const topics = [
     { id: "1", title: "html", body: "html is ..." },
     { id: "2", title: "css", body: "css is ..." },
     { id: "3", title: "javascript", body: "js is ..." }
   ]
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB" />
+  } else if (mode === 'READ') {
+    content = <Article title="Read" body="Hello, Read" />
+  }
   return (
     <div >
-      <Header title="WEB" onChangeMode={function () {
-        alert('Header');
+      <Header title="WEB" onChangeMode={() => {
+        mode = 'WELCOME'
       }}
       />
-      <Nav topics={topics} />
-      <Article title="Welcome" body="Hello, WEB" />
+      <Nav topics={topics} onChangeMode={(id) => {
+        mode = 'READ'
+      }} />
+      {content}
 
     </div >
   );
